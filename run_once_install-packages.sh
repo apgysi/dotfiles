ba220e0bb7e8a5d1c62d7523c27a66fcaad03b00
apgysi <alexander.gysi@nmt.edu>
2022-07-30 20:01:18 -0600

    new scripts

---
+++ run_once_install-packages.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+pkglist="
+vim
+ranger
+libreoffice-still
+gimp
+inkscape
+thunderbird
+texstudio
+texlive-most
+hyphen-en
+hunspell-en_us
+"
+
+echo -e "$pkglist" > pkglist
+
+sudo pacman -S --noconfirm --needed - < pkglist;
+
+read -p "Do you want to install AUR packages (y/n) " yn
+
+pkglist_aur="
+veusz
+zoom
+mendeleydesktop
+ttf-ms-fonts
+"
+
+echo -e "$pkglist_aur" > pkglist_aur
+
+mkdir -p ~/.build
+
+yay --builddir ~/.build --save
+
+case $yn in
+	y )	yay -S --noconfirm --needed - < pkglist_aur;;
+	* ) echo continue;;
+esac
