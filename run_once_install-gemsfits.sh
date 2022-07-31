8bbe86338f019c74f65d1b7ea8e4b6e15380ea12
apgysi <alexander.gysi@nmt.edu>
2022-07-30 20:52:52 -0600

    aur and gemsfits script commit

---
+++ run_once_install-gemsfits.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+mkdir gitGEMSFITS 
+cd gitGEMSFITS
+mkdir gemsfits standalone
+gemsfits git clone https://bitbucket.org/gems4/gemsfits.git .
+cd ..
+cd standalone
+git clone https://bitbucket.org/gems4/gems3k.git .
+git checkout dev-gemsfits
+cd 
+
+cd Downloads
+git clone https://github.com/Softmotions/ejdb.git
+cd ejdb
+git checkout v1.2.12
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../
+make
+sudo make install
+
+wget https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-0.6.3.zip
+
+unzip yaml-cpp-0.6.3.zip
+
+cd yaml-cpp-0.6.3
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
